b6229e81775a18781aea16a52bbd9590d6bd2bb4
Gulshat5510 <gulshataganowa02@gmail.com>
2022-03-15 15:44:10 +0500

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <router-link to="/">
+    <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+  <router-view />
+  </main>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans' , sans-serif;
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
+}
+
+body {
+  background-color: #35495E;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2C3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  color: #FFF;
+  font-size: 28px;
+
+  span {
+    color: #42B883;
+  }
+}
+</style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+    apikey: "b393cbcc"
+}
